23dd60db2323aeb8a057e699db32a67c09564a26
Susumu Yamazaki <zacky1972@gmail.com>
2023-10-19 05:40:10 +0900

    enhance: implement full adder

--- src/adder.sv
+++ src/adder.sv
@@ -1,11 +1,13 @@
 module adder(
     input logic a,
     input logic b,
-    output logic c
+    output logic c,
+    output logic d
 );
 
     always_comb begin
         c = a + b;
+        d = a & b;
     end
 
 endmodule
