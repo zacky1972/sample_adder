d52a4a587b3f86a0c3eccb7446c906f404403654
Susumu Yamazaki <zacky1972@gmail.com>
2023-10-19 05:40:10 +0900

    enhance: implement full adder

--- test/adder_test.sv
+++ test/adder_test.sv
@@ -2,18 +2,19 @@ module adder_test();
     logic a;
     logic b;
     logic c;
+    logic d;
 
-    adder dut(a, b, c);
+    adder dut(a, b, c, d);
 
     initial begin
         a = 0; b = 0; #10;
-        assert (c === 0) else $error("adder: (a: %h, b: %h, c: %h)", a, b, c);
+        assert (c === 0 && d == 0) else $error("adder: (a: %h, b: %h, c: %h, d: %h)", a, b, c, d);
         a = 0; b = 1; #10;
-        assert (c === 1) else $error("adder: (a: %h, b: %h, c: %h)", a, b, c);
+        assert (c === 1 && d == 0) else $error("adder: (a: %h, b: %h, c: %h, d: %h)", a, b, c, d);
         a = 1; b = 0; #10;
-        assert (c === 1) else $error("adder: (a: %h, b: %h, c: %h)", a, b, c);
+        assert (c === 1 && d == 0) else $error("adder: (a: %h, b: %h, c: %h, d: %h)", a, b, c, d);
         a = 1; b = 1; #10;
-        assert (c === 0) else $error("adder: (a: %h, b: %h, c: %h)", a, b, c);
+        assert (c === 0 && d == 1) else $error("adder: (a: %h, b: %h, c: %h, d: %h)", a, b, c, d);
     end
     
 endmodule
